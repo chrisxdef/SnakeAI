9f27e199e5382bd1de8451889095d0c5a1274c55
Chris Heisler <cheisler72@gmail.com>
2018-02-21 15:20:29 +0000

    added position of head relative to senter of the snake as a heuristic dimension.

--- src/snake_game.py
+++ src/snake_game.py
@@ -1,62 +1,59 @@
 from time import sleep
 from random import *
-from Tkinter import *
 import json
 import operator
 import math
 
-class SnakeGame(Tk):
+class SnakeGame():
     UP=-20
     RIGHT=1
     DOWN=20
     LEFT=-1
     def __init__(self, *args, **kwargs):
-	Tk.__init__(self, *args, **kwargs)
         self.width = 20
-	self.bias = [1.0, 1.0, 1.0, 1.0]
-	self.key = "1.0:1.0:1.0:1.0"
-	self.highscores = []
+        self.bias = [1.0, 1.0, 1.0, 1.0,1.0]
+        self.key = "1.0:1.0:1.0:1.0:1.0"
+        self.highscores = []
         self.height = 20
         self.board = self.width*self.height*[0]
-	self.score = 0
+        self.score = 0
         self.head = 22
         self.snake = [self.head,self.head+self.width,self.head+(2*self.width)]
         self.food = [10]
         self.last_input = None
-       	self.scale = 16
-	self.title("SnA*ke")
-	self.grid = []
-	self.sidebar = Frame(self, width=200, bg='white', height=500, relief='sunken', borderwidth=2)
-	self.sidebar.pack(expand=True, fill='y', side='left', anchor='nw')	
-	self.mainarea = Frame(self, bg='#CCC', width=500, height=500)
-	self.mainarea.pack(expand=True, fill='both', side='right')
-	self.w = Canvas(self.mainarea, width=self.width*self.scale, height=self.height*self.scale)
-	self.w.pack()
-	for i in range(self.width*self.height):
-	    x= (i%self.width)*self.scale
-	    y= (i/self.width)*self.scale
-	    self.grid.append(self.w.create_rectangle(x,y,x+self.scale,y+self.scale, fill="white"))
-	self.scoreboard = Label(self.sidebar, text="0")
-	self.scoreboard.pack()
-	self.on = True
+        self.scale = 16
+        #self.title("SnA*ke")
+        #self.grid = []
+        #self.sidebar = Frame(self, width=200, bg='white', height=500, relief='sunken', borderwidth=2)
+        #self.sidebar.pack(expand=True, fill='y', side='left', anchor='nw')     
+        #self.mainarea = Frame(self, bg='#CCC', width=500, height=500)
+        #self.mainarea.pack(expand=True, fill='both', side='right')
+        #self.w = Canvas(self.mainarea, width=self.width*self.scale, height=self.height*self.scale)
+        #self.w.pack()
+        #for i in range(self.width*self.height):
+        #    x= (i%self.width)*self.scale
+        #    y= (i/self.width)*self.scale
+        #    self.grid.append(self.w.create_rectangle(x,y,x+self.scale,y+self.scale, fill="white"))
+        #self.scoreboard = Label(self.sidebar, text="0")
+        #self.scoreboard.pack()
+        self.on = True
 
-	self.population = []
-	self.initPopulation()
+        self.population = []
+        self.initPopulation()
 
-	self.c = 0
-	
-	self.redraw(1)
+        self.c = 0
+
 
     def update(self,cmd):
-	self.c+=1
-	if self.c == 400:
-	    print("caught in loop")
-	    self.gameover()
-	    self.c = 0;
- 	    return
-	if cmd == None:
-	    cmd = self.last_input
-	self.last_input = cmd
+        self.c+=1
+        if self.c == 400:
+            print("caught in loop")
+            self.gameover()
+            self.c = 0;
+            return
+        if cmd == None:
+            cmd = self.last_input
+        self.last_input = cmd
         #Update head location and check for wall collision
         if cmd==SnakeGame.UP and self.head>self.width-1:
             self.head-=self.width
@@ -75,12 +72,12 @@ class SnakeGame(Tk):
         #If the snake head is on a food tile then retain the end of the snake body
         #and removee the food from the board
         if self.head in self.food:
-	    self.c = 0
-	    self.score += 1
+            self.c = 0
+            self.score += 1
             self.food.remove(self.head)
             r = randint(0,399)
-	    while r in self.snake or r in self.food:
-        	r = randint(0,399)
+            while r in self.snake or r in self.food:
+                r = randint(0,399)
             self.food.append(r)
         else:
             self.snake.pop()
@@ -113,24 +110,24 @@ class SnakeGame(Tk):
         
         
     def idle(self):            
-	h = self.head
-	moves = []
-	if h>=self.width and not h-self.width in self.snake:
-		moves.append(SnakeGame.UP)
-	if h%self.width!=self.width-1 and not h+1 in self.snake:
-		moves.append(SnakeGame.RIGHT)
-	if h+self.width<self.height*(self.width-1) and not h+self.width in self.snake:
-		moves.append(SnakeGame.DOWN)
-	if h%self.width!=0 and not h-1 in self.snake:
-		moves.append(SnakeGame.LEFT)
-	if not moves:
-		return None
-	best = 9999
-	for i in moves:
-		if self.h(i+h) < best:
-			best = i
-	return best
-	
+        h = self.head
+        moves = []
+        if h>=self.width and not h-self.width in self.snake:
+                moves.append(SnakeGame.UP)
+        if h%self.width!=self.width-1 and not h+1 in self.snake:
+                moves.append(SnakeGame.RIGHT)
+        if h+self.width<self.height*(self.width-1) and not h+self.width in self.snake:
+                moves.append(SnakeGame.DOWN)
+        if h%self.width!=0 and not h-1 in self.snake:
+                moves.append(SnakeGame.LEFT)
+        if not moves:
+                return None
+        best = 9999
+        for i in moves:
+                if self.h(i+h) < best:
+                        best = i
+        return best
+        
 
     def astar(self,frontier,closed, best=None):
         if not frontier:
@@ -141,19 +138,19 @@ class SnakeGame(Tk):
             if p[0] < shortest[0]:
                 shortest = p
         frontier.remove(shortest)
-	if best == None:
-	    best = shortest
-	elif shortest[1]-shortest[0] < best[1]-best[0]:
-	    best = shortest
+        if best == None:
+            best = shortest
+        elif shortest[1]-shortest[0] < best[1]-best[0]:
+            best = shortest
         #Expand upon the shortest path
         #Do not include paths that lead into a wall, snake or indexes in closed
         #When a possible path is found update its score and add it to the frontier
-	#Update snake
-	snake = self.snake[:]
-	for i in shortest[2:]:
-		snake.pop()
-		snake.insert(0, i)
-	
+        #Update snake
+        snake = self.snake[:]
+        for i in shortest[2:]:
+                snake.pop()
+                snake.insert(0, i)
+        
         #Expansion of shortest path BEGIN
         #End of shortest path index
         n = shortest[len(shortest)-1]
@@ -192,27 +189,39 @@ class SnakeGame(Tk):
 
         
     def h(self, n, snake=None):
-	if snake == None:
-	    snake = self.snake
-	#Implement a heuristic that uses the position of the snake
-	min_row = self.height
-	max_row = 0
-	min_col = self.width
-	max_col = 0
-	for i in snake:
-	    row = i/self.width
-	    col = i%self.width
-	    if row > max_row:
-		max_row = row
-	    if row < min_row:
-		min_row = row
-	    if col > max_col:
-		max_col = col
-	    if col < min_col:
-		min_col = col
-	rect_w = max_col + min_col + 1
-	rect_h = max_row + min_row + 1
-	rect_a = rect_w * rect_h
+        if snake == None:
+            snake = self.snake
+        #Implement a heuristic that uses the position of the snake
+        min_row = self.height
+        max_row = 0
+        min_col = self.width
+        max_col = 0
+        for i in snake:
+            row = i/self.width
+            col = i%self.width
+            if row > max_row:
+                max_row = row
+            if row < min_row:
+                min_row = row
+            if col > max_col:
+                max_col = col
+            if col < min_col:
+                min_col = col
+        rect_w = max_col + min_col + 1
+        rect_h = max_row + min_row + 1
+        rect_a = rect_w * rect_h
+
+        #get the number of cols away head is from rect
+        m = snake[0]%self.width
+        m_cols = abs(max_col - m)
+        if m_cols < abs(min_col - m):
+            m_cols = abs(min_col - m)
+        #and rows
+        m_rows = abs(max_row - m)
+        if m_rows < abs(min_row - m):
+            m_rows = abs(min_row - m)
+        h_edge = m_rows + m_cols
+
 
         h_rows = self.height*self.width
         h_cols = self.height*self.width
@@ -225,146 +234,148 @@ class SnakeGame(Tk):
                 h_rows = rows
                 h_cols = cols
 
-	#Add 1 if tile is near a wall
-	h_wall = 0.0
-	if n % self.width == 0:
-	    h_wall+=1
-	if n % self.width == self.width-1:
-	    h_wall+=1
-	if n < self.width:
-	    h_wall+=1
-	if n / self.width == self.height-1:
-	    h_wall+=1
+        #Add 1 if tile is near a wall
+        h_wall = 0.0
+        if n % self.width == 0:
+            h_wall+=1
+        if n % self.width == self.width-1:
+            h_wall+=1
+        if n < self.width:
+            h_wall+=1
+        if n / self.width == self.height-1:
+            h_wall+=1
 
-	#how much free space around this point
-	h_free = 0.0
-	sqre = int( math.ceil(math.sqrt(len(self.snake))/2.0) )
-	for i in range(n-(sqre)-(sqre*self.width),n+(sqre)+(sqre*self.width)):
-		if i in snake or i<n and i%self.width>n%self.width and i%self.width>(n+sqre)%self.width or i>n and i%self.width<n%self.width and i%self.width<(n+sqre)%self.width:
-			h_free+=1
+        #how much free space around this point
+        h_free = 0.0
+        sqre = int( math.ceil(math.sqrt(len(self.snake))/2.0) )
+        for i in range(n-(sqre)-(sqre*self.width),n+(sqre)+(sqre*self.width)):
+                if i in snake or i<n and i%self.width>n%self.width and i%self.width>(n+sqre)%self.width or i>n and i%self.width<n%self.width and i%self.width<(n+sqre)%self.width:
+                        h_free+=1
 
-        return float(h_cols+h_rows) * self.bias[0] + float(rect_a-len(snake)) * self.bias[1] + h_wall * self.bias[2] + h_free * self.bias[3]
+        return float(h_cols+h_rows) * self.bias[0] + float(rect_a-len(snake)) * self.bias[1] + h_wall * self.bias[2] + h_free * self.bias[3] + float(h_edge) * self.bias[4]
                 
     def gameover(self):
-	self.c = 0
+        self.c = 0
         print("Game Over")
-	self.setHighscore()
+        self.setHighscore()
         self.setBias(self.population.pop())
-	if not self.population:
-	    self.nextGeneration()
-	self.score = 0
+        if not self.population:
+            self.nextGeneration()
+        self.score = 0
         self.head = 22
         self.snake = [self.head,self.head+self.width,self.head+(2*self.width)]
-        self.food = [10]
-	    
-	
+        self.food = [randint(0, self.height*self.width-1)]
+        print(len(self.population)) 
+        
 
-    def redraw(self, delay):
-	self.scoreboard.configure(text=str(self.score))
-	if self.on:	
-		self.update(self.ai())
-		for i in range(len(self.board)):
-			color = "white"
-			if i in self.snake:
-			    color = "black"
-			elif i in self.food:
-			    color = "gray"
-			self.w.itemconfig(self.grid[i], fill=color)
-					
-		self.after(delay, lambda: self.redraw(delay))
+    def mainloop(self):
+        #self.scoreboard.configure(text=str(self.score))
+        while(self.on):     
+                self.update(self.ai())
+        #       for i in range(len(self.board)):
+        #               color = "white"
+        #               if i in self.snake:
+        #                   color = "black"
+        #               elif i in self.food:
+        #                   color = "gray"
+        #               self.w.itemconfig(self.grid[i], fill=color)
+                                        
 
     def setBias(self, bias):
-	self.bias = bias[:]
-	self.key = str(self.bias[0]) + ':' + str(self.bias[1]) + ':' + str(self.bias[2]) + ':' + str(self.bias[3])
+        self.bias = bias[:]
+        self.key=''
+        for i in range(len(self.bias)-1):
+            self.key += str(self.bias[1]) + ':'
+        self.key += str(self.bias[len(self.bias)-1])
 
    # record a score and sore by best
     def setHighscore(self):
-	print("Snake " + self.key)
-	with open('./highscores.json', 'r') as hs:
-	    data = json.loads(hs.read())
-	    hs.close()
-	if self.key in data.keys():
-	    if data[self.key] >= self.score:
-		print("Score " + str(self.score))
-		return
-	print("Highscore " + str(self.score))
-	data[self.key] = self.score
-	sorted_data = sorted(data.items(), key=operator.itemgetter(1))
-	self.highscores = []
-	k = 5
-	if len(sorted_data) < k:
-		k = len(sorted_data)
-	for i in range(k):
-		self.highscores.append(sorted_data[i])
-	with open('./highscores.json', 'w') as hs:
-	    hs.write(str(json.dumps(data)))
-	    hs.close()
-	
+        print("Snake " + self.key)
+        with open('./highscores.json', 'r') as hs:
+            data = json.loads(hs.read())
+            hs.close()
+        if self.key in data.keys():
+            if data[self.key] >= self.score:
+                print("Score " + str(self.score))
+                return
+        print("Highscore " + str(self.score))
+        data[self.key] = self.score
+        sorted_data = sorted(data.items(), key=operator.itemgetter(1))
+        self.highscores = []
+        k = 5
+        if len(sorted_data) < k:
+                k = len(sorted_data)
+        for i in range(k):
+                self.highscores.append(sorted_data[i])
+        with open('./highscores.json', 'w') as hs:
+            hs.write(str(json.dumps(data)))
+            hs.close()
+        
     def getTopN(self, n):
-	with open('./highscores.json', 'r') as hs:
-	    data = json.loads(hs.read())
-	    hs.close()
-	sorted_data = sorted(data.items(), key=operator.itemgetter(1))
-	self.highscores = []
-	if len(sorted_data) < n:
-		n = len(sorted_data)
-	self.highscores = []
-	for i in range(n):
-		self.highscores.append(sorted_data[i])
-	
+        with open('./highscores.json', 'r') as hs:
+            data = json.loads(hs.read())
+            hs.close()
+        sorted_data = sorted(data.items(), key=operator.itemgetter(1))
+        self.highscores = []
+        if len(sorted_data) < n:
+                n = len(sorted_data)
+        self.highscores = []
+        for i in range(n):
+                self.highscores.append(sorted_data[i])
+        
 
     def getHighscore(self, key=None, bias=None):
-	if key == None:
-	    key = self.key
-	if bias == None:
-	    bias = self.bias[:]
-	with open('./highscores.json', 'r') as hs:
-	    data = json.loads(hs.read())
-	    hs.close()
-	if key in data.keys():
-	   return data[key]
-	return None
+        if key == None:
+            key = self.key
+        if bias == None:
+            bias = self.bias[:]
+        with open('./highscores.json', 'r') as hs:
+            data = json.loads(hs.read())
+            hs.close()
+        if key in data.keys():
+           return data[key]
+        return None
 
 
     def initPopulation(self):
-	for i in range(1000):
-	    bias = []
-	    for i in range(len(self.bias)):
-		bias.append(random())
-	    self.population.append(bias)
-		
+        for i in range(1000):
+            bias = []
+            for i in range(len(self.bias)):
+                bias.append(random())
+            self.population.append(bias)
+                
 
     def nextGeneration(self):
-	print("new Gen")
-	pop = []
-	top = self.getTopN(500)
-	self.population = []
-	for i in range(500):
-	    pair = []
-	    for i in range(10):
-		r = choice(top)
-		if r[1] > pair[0][1]:
-		    pair.insert(0, r)
-	    pair = self.nextPop([pair[0][0], pair[1][0]])
-	    pop.append(pair[0])
-	    pop.append(pair[1])
-	    
+        print("new Gen")
+        pop = []
+        top = self.getTopN(500)
+        self.population = []
+        for i in range(500):
+            pair = []
+            for i in range(10):
+                r = choice(top)
+                if r[1] > pair[0][1]:
+                    pair.insert(0, r)
+            pair = self.nextPop([pair[0][0], pair[1][0]])
+            pop.append(pair[0])
+            pop.append(pair[1])
+            
     def nextPop(self, pair):
-	first = pair[0].split(':')
-	second = pair[1].split(':')
-	pair = [first, second]
-	new_bias1 = []
-	new_bias2 = []
-	for i in range(len(first)):
-	    r = nextInt(2)
-	    new_bias1.append(float(pair[r][i]))
-	    r = (r+1)%2
-	    new_bias2.append(float(pair[r][i]))
-	if random() < 0.2:
-	    new_bias1[randInt(len(new_bias))] = choice(new_bias) * random()
-	if random() < 0.2:
-	    new_bias2[randInt(len(new_bias))] = choice(new_bias) * random()
-	return [new_bias1, new_bias2]
+        first = pair[0].split(':')
+        second = pair[1].split(':')
+        pair = [first, second]
+        new_bias1 = []
+        new_bias2 = []
+        for i in range(len(first)):
+            r = nextInt(2)
+            new_bias1.append(float(pair[r][i]))
+            r = (r+1)%2
+            new_bias2.append(float(pair[r][i]))
+        if random() < 0.2:
+            new_bias1[randInt(len(new_bias))] = choice(new_bias) * random()
+        if random() < 0.2:
+            new_bias2[randInt(len(new_bias))] = choice(new_bias) * random()
+        return [new_bias1, new_bias2]
  
 
 
