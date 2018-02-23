bd13606e2f6ce715448c6428c1ec8e24d7519086
Christopher Heisler <cheisler72@gmail.com>
2018-02-23 17:17:47 -0500

    New getAdjacent function and new Heuristic values
    Holy cow! This snake got a high score of 117!

--- src/snake_game.py
+++ src/snake_game.py
@@ -4,51 +4,51 @@ from random import choice
 from Tkinter import *
 
 class SnakeGame(Tk):
-    UP=0
+    UP=-20
     RIGHT=1
-    DOWN=2
-    LEFT=3
+    DOWN=20
+    LEFT=-1
     def __init__(self, *args, **kwargs):
-	Tk.__init__(self, *args, **kwargs)
+        Tk.__init__(self, *args, **kwargs)
         self.width = 20
         self.height = 20
         self.board = self.width*self.height*[0]
-	self.score = 0
+        self.score = 0
         self.head = 22
         self.snake = [self.head,self.head+self.width,self.head+(2*self.width)]
         self.food = [10]
         self.last_input = None
-       	self.scale = 16
-	self.title("SnA*ke")
-	self.grid = []
-	self.sidebar = Frame(self, width=200, bg='white', height=500, relief='sunken', borderwidth=2)
-	self.sidebar.pack(expand=True, fill='y', side='left', anchor='nw')	
-	self.mainarea = Frame(self, bg='#CCC', width=500, height=500)
-	self.mainarea.pack(expand=True, fill='both', side='right')
-	self.w = Canvas(self.mainarea, width=self.width*self.scale, height=self.height*self.scale)
-	self.w.pack()
-	for i in range(self.width*self.height):
-	    x= (i%self.width)*self.scale
-	    y= (i/self.width)*self.scale
-	    self.grid.append(self.w.create_rectangle(x,y,x+self.scale,y+self.scale, fill="white"))
-	self.scoreboard = Label(self.sidebar, text="0")
-	self.scoreboard.pack()
-	self.on = True
-	self.redraw(100)
+        self.scale = 16
+        self.title("SnA*ke")
+        self.grid = []
+        self.sidebar = Frame(self, width=200, bg='white', height=500, relief='sunken', borderwidth=2)
+        self.sidebar.pack(expand=True, fill='y', side='left', anchor='nw')  
+        self.mainarea = Frame(self, bg='#CCC', width=500, height=500)
+        self.mainarea.pack(expand=True, fill='both', side='right')
+        self.w = Canvas(self.mainarea, width=self.width*self.scale, height=self.height*self.scale)
+        self.w.pack()
+        for i in range(self.width*self.height):
+            x= (i%self.width)*self.scale
+            y= (i/self.width)*self.scale
+            self.grid.append(self.w.create_rectangle(x,y,x+self.scale,y+self.scale, fill="white"))
+        self.scoreboard = Label(self.sidebar, text="0")
+        self.scoreboard.pack()
+        self.on = True
+        self.redraw(5)
 
     def update(self,cmd):
-	if cmd == None:
-	    cmd = self.last_input
-	self.last_input = cmd
+        if cmd == None:
+            cmd = self.last_input
+        self.last_input = cmd
         #Update head location and check for wall collision
         if cmd==SnakeGame.UP and self.head>self.width-1:
-            self.head-=self.width
+            self.head+=SnakeGame.UP
         elif cmd==SnakeGame.RIGHT and self.head%self.width!=self.width-1:
-            self.head+=1
-        elif cmd==SnakeGame.DOWN and self.head<((self.width-1)*self.height)-1:
-            self.head+=self.width
+            self.head+=SnakeGame.RIGHT
+        elif cmd==SnakeGame.DOWN and self.head/self.width!=self.height-1:
+            self.head+=SnakeGame.DOWN
         elif cmd==SnakeGame.LEFT and self.head%self.width!=0:
-            self.head-=1
+            self.head+=SnakeGame.LEFT
         else:
             self.gameover()
             return
@@ -58,11 +58,11 @@ class SnakeGame(Tk):
         #If the snake head is on a food tile then retain the end of the snake body
         #and removee the food from the board
         if self.head in self.food:
-	    self.score += 1
+            self.score += 1
             self.food.remove(self.head)
             r = randint(0,399)
-	    while r in self.snake or r in self.food:
-        	r = randint(0,399)
+            while r in self.snake or r in self.food:
+                r = randint(0,399)
             self.food.append(r)
         else:
             self.snake.pop()
@@ -75,13 +75,57 @@ class SnakeGame(Tk):
         #Add the new snake head to the snake
         self.snake.insert(0,self.head)
 
+    def getAdjacent(self, n, snake=None):
+        if snake == None:
+            snake = self.snake
+        adj = []
+        #Up
+        up = n+SnakeGame.UP
+        if not up<0 and not up in snake:
+            adj.append(up)
+        #Down
+        down = n+SnakeGame.DOWN
+        if not down>=self.width*self.height and not down in snake:
+            adj.append(down)
+        #Left
+        left = n+SnakeGame.LEFT
+        if not left%self.width==self.width-1 and not left in snake:
+            adj.append(left)
+        #Right
+        right = n+SnakeGame.RIGHT
+        if not right%self.width==0 and not right in snake:
+            adj.append(right)
+        return adj
+
+    def getAllAdjacent(self, n, snake=None):
+        if snake == None:
+            snake = self.snake
+        frontier = [n]
+        closed = []
+        while frontier:
+            if len(closed) > len(snake)*2:
+                return closed
+            k = frontier.pop()
+            closed.append(k)
+            adj = self.getAdjacent(k, snake)
+            for i in adj:
+                if not i in closed:
+                    frontier.append(i)
+        return closed
+
+    def countAdjacent(self, n, snake=None):
+        if snake == None:
+            snake = self.snake
+        return len(self.getAllAdjacent(n, snake))
+        
+
     def ai(self):
-        frontier = [[0,self.head]]
+        frontier = [[9999,self.head]]
         closed = []
         path = self.astar(frontier,closed)
         if path == None or len(path)<3:
-            return self.idle() 
-	print(path)
+            print(len(path))
+            return self.idle()
         move = path[2]
         diff = self.head - move
         if diff == self.width:
@@ -92,49 +136,57 @@ class SnakeGame(Tk):
             return SnakeGame.RIGHT
         if diff == 1:
             return SnakeGame.LEFT
-        return None
-        
+        return diff
         
     def idle(self):            
-	print "idled"
-	h = self.head
-	moves = []
-	if h-self.width>0 and not h-self.width in self.snake:
-		moves.append(SnakeGame.UP)
-	if h%self.width!=self.width and not h+1 in self.snake:
-		moves.append(SnakeGame.RIGHT)
-	if h+self.width<self.height*self.width-1 and not h+self.width in self.snake:
-		moves.append(SnakeGame.DOWN)
-	if h%self.width!=0 and not h-1 in self.snake:
-		moves.append(SnakeGame.LEFT)
-	if not moves:
-		return None
-	score = 9999
-	best = None
-	for i in moves:
-		if self.h(i+h) < best:
-			best = i
-	return best
-	
+        h = self.head
+        print("idle")
+        moves = []
+        #UP
+        if h>=self.width and not h+SnakeGame.UP in self.snake:
+                moves.append(SnakeGame.UP)
+        #RIGHT
+        if h%self.width!=self.width-1 and not h+SnakeGame.RIGHT in self.snake:
+                moves.append(SnakeGame.RIGHT)
+        #DOWN
+        if h/self.width!=self.height-1 and not h+SnakeGame.DOWN in self.snake:
+                moves.append(SnakeGame.DOWN)
+        #LEFT
+        if h%self.width!=0 and not h+SnakeGame.LEFT in self.snake:
+                moves.append(SnakeGame.LEFT)
+        if not moves:
+                return None
+        best = 9998
+        for i in moves:
+                if self.h(i+h) < best:
+                        best = i
+        return best
+    
 
-    def astar(self,frontier,closed):
+    def astar(self,frontier,closed, best=None):
         if not frontier:
-            return None
+            return best
         #Explore frontier for shortest path
         shortest = frontier[0]
         for p in frontier:
             if p[0] < shortest[0]:
                 shortest = p
         frontier.remove(shortest)
+
+        if best == None:
+            best = shortest
+        elif shortest[0]-len(shortest) < best[0]-len(best):
+            best = shortest
+
         #Expand upon the shortest path
         #Do not include paths that lead into a wall, snake or indexes in closed
         #When a possible path is found update its score and add it to the frontier
-	#Update snake
-	snake = self.snake[:]
-	for i in shortest[2:]:
-		snake.pop()
-		snake.insert(0, i)
-	
+        #Update snake
+        snake = self.snake[:]
+        for i in shortest[2:]:
+            snake.pop()
+            snake.insert(0, i)
+    
         #Expansion of shortest path BEGIN
         #End of shortest path index
         n = shortest[len(shortest)-1]
@@ -169,34 +221,53 @@ class SnakeGame(Tk):
                 right.append(n+1)
                 frontier.append(right)
             #END
-        return self.astar(frontier,closed)
+        return self.astar(frontier,closed,best)
 
         
     def h(self, n, snake=None):
-	if snake == None:
-	    snake = self.snake
-	#Implement a heuristic that uses the position of the snake
-	min_row = self.height
-	max_row = 0
-	min_col = self.width
-	max_col = 0
-	for i in snake:
-	    row = i/self.width
-	    col = i%self.width
-	    if row > max_row:
-		max_row = row
-	    if row < min_row:
-		min_row = row
-	    if col > max_col:
-		max_col = col
-	    if col < min_col:
-		min_col = col
-	rect_w = max_col + min_col + 1
-	rect_h = max_row + min_row + 1
-	rect_a = rect_w * rect_h
+        if len(self.getAdjacent(n, snake))==0:
+            return 9999
+        if snake == None:
+            snake = self.snake
+        #Implement a heuristic that uses the position of the snake
+        #Build rectangle around snake and find rect's area
+        min_row = self.height
+        max_row = 0
+        min_col = self.width
+        max_col = 0
+        for i in snake:
+            row = i/self.width
+            col = i%self.width
+            if row > max_row:
+                max_row = row
+            if row < min_row:
+                min_row = row
+            if col > max_col:
+                max_col = col
+            if col < min_col:
+                min_col = col
+        rect_w = max_col + min_col + 1
+        rect_h = max_row + min_row + 1
+        rect_a = rect_w * rect_h
 
+       #get the number of cols away head is from rect
+        m = snake[0]%self.width
+        m_cols = abs(max_col - m)
+        if m_cols < abs(min_col - m):
+            m_cols = abs(min_col - m)
+        #and rows
+        m_rows = abs(max_row - m)
+        if m_rows < abs(min_row - m):
+            m_rows = abs(min_row - m)
+        h_edge = m_rows + m_cols
+
+
+        #distance to food
         h_rows = self.height*self.width
         h_cols = self.height*self.width
+        if not food:
+            h_rows = 0
+            h_cols = 0
         for f in self.food:
             #number of rows from n to f
             rows = abs(n/self.width-f/self.width)
@@ -205,26 +276,32 @@ class SnakeGame(Tk):
             if rows<h_rows and cols<h_cols:
                 h_rows = rows
                 h_cols = cols
-        return h_cols + h_rows + (rect_a-len(snake))
-                
+
+        #Count all tiles adjacent to this tile and adjacent to those
+        h_adj = self.countAdjacent(n, snake)
+
+        
+        
+        return (h_cols + h_rows) + ((rect_a-len(snake)) + (len(snake) - h_adj) + h_edge)*2
+
     def gameover(self):
         print("Game Over")
-	print(self.last_input)
+        print(self.last_input)
         self.on = False
 
     def redraw(self, delay):
-	self.scoreboard.configure(text=str(self.score))
-	if self.on:	
-		self.update(self.ai())
-		for i in range(len(self.board)):
-			color = "white"
-			if i in self.snake:
-			    color = "black"
-			elif i in self.food:
-			    color = "gray"
-			self.w.itemconfig(self.grid[i], fill=color)
-					
-		self.after(delay, lambda: self.redraw(delay))
+        self.scoreboard.configure(text=str(self.score))
+        if self.on: 
+            self.update(self.ai())
+            for i in range(len(self.board)):
+                color = "white"
+                if i in self.snake:
+                    color = "black"
+                elif i in self.food:
+                    color = "gray"
+                self.w.itemconfig(self.grid[i], fill=color)
+                        
+            self.after(delay, lambda: self.redraw(delay))
 
 if __name__ == '__main__':
     game = SnakeGame()
